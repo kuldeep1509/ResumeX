f43060ba8ee1d00f0de3a6853603f6b3d3e03b92
Kuldeep Jadeja <kuldeepsinh.jadeja@thegatewaycorp.co.in>
2026-04-14 15:47:16 +0530

    refactor: update route matcher for protected routes and improve async handling in middleware

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,16 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/dashboard", "/my-resume/:resumeId/edit"]);
+const isProtectedRoute = createRouteMatcher([
+  "/dashboard(.*)",
+  "/my-resume/(.*)/edit",
+]);
 
-export default clerkMiddleware((auth, request) => {
+export default clerkMiddleware(async (auth, request) => {
   if (isProtectedRoute(request)) {
-    auth().protect();
+    await auth.protect();
   }
 });
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-  unstable_allowDynamic: [
-    "**/node_modules/@clerk/shared/dist/**",
-    "**/node_modules/@clerk/nextjs/dist/**",
-    "**/node_modules/@clerk/clerk-react/dist/**",
-  ],
 };
